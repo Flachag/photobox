b7d30945c84705c1621960e2f843135f2a5597ee
Jules Sayer <jules.sayer@protonmail.com>
2020-02-24 23:55:03 +0100

    fix

--- js/photobox.js
+++ js/photobox.js
@@ -2,7 +2,7 @@ import {init, load} from "./gallery.js";
 
 $(function () {
     init(1);
-    $('load').on('click', function () {
+    $('#load').on('click', function () {
         load();
     });
 });
