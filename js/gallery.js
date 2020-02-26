a0b5174cc488868433c88b31b0b1b835bfd79861
Jules Sayer <jules.sayer@protonmail.com>
2020-02-26 19:30:49 +0100

    first try lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -9,9 +9,33 @@ function insert(data, base_url) {
             $('img', card).attr({
                 alt: photo.photo.titre,
                 title: photo.photo.titre,
-                src: base_url + photo.photo.thumbnail.href
+                src: base_url + photo.photo.thumbnail.href,
+                'data-image': base_url + photo.photo.original.href
             });
             $('.row').append(card);
+
+            card.find('img').on('click', function () {
+                let image_href = $(this).attr("data-image");
+                $('#content').html('<img src="' + image_href + '" />');
+                if ($('#lightbox').length > 0) {
+
+                    $('#content').html('<img src="' + image_href + '" />');
+
+                    $('#lightbox').show();
+                } else {
+                    let lightbox =
+                        '<div id="lightbox">' +
+                        '<p>Click to close</p>' +
+                        '<div id="content">' +
+                        '<img src="' + image_href +'" />' +
+                        '</div>' +
+                        '</div>';
+                    $('body').append(lightbox);
+                }
+                $('#lightbox').on('click', function() { //must use live, as the lightbox element is inserted into the DOM
+                    $('#lightbox').hide();
+                });
+            });
         });
     }
 }
